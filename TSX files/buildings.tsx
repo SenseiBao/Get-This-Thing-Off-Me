be2e1cf0f1580b42e1597c24af4f1aaae570684f
Bao Pham <47954774+SenseiBao@users.noreply.github.com>
2025-04-18 00:25:30 -0400

    Moved around some files

--- TSX files/buildings.tsx
+++ TSX files/buildings.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="buildings" tilewidth="8" tileheight="8" tilecount="1702" columns="46">
- <image source="assets/tilesets/buildings.png" width="368" height="296"/>
+ <image source="../assets/tilesets/buildings.png" width="368" height="296"/>
 </tileset>
--- TSX files/grass.tsx
+++ TSX files/grass.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="grass" tilewidth="8" tileheight="8" tilecount="4" columns="4">
- <image source="assets/tilesets/grass.png" width="32" height="8"/>
+ <image source="../assets/tilesets/grass.png" width="32" height="8"/>
  <tile id="3" probability="10"/>
 </tileset>
