b51cc6215cfd70aa4b12044f9b539f0ee19c8ad6
Quorious-Betsy <76961953+Quorious-Betsy@users.noreply.github.com>
2025-04-22 02:29:09 -0400

    Judas House and tilesets
    Added Judas house to backgrounds. Added used tilesets for interior decoration. Added link to relevant websites for tilesets

---
+++ TSX files/Wall_Edges.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="Wall_Edges" tilewidth="8" tileheight="8" tilecount="4" columns="4">
+ <image source="../assets/tilesets/Wall_Edges.png" trans="65ff00" width="32" height="8"/>
+</tileset>
