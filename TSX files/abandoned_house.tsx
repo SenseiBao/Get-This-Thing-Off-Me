781cd6b3268c01f04d93fedfa1d72110f0617602
Quorious-Betsy <76961953+Quorious-Betsy@users.noreply.github.com>
2025-04-28 18:38:17 -0400

    Added used TSX files to the project folder. Added credits for used tilesets

---
+++ TSX files/abandoned_house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="abandoned_house" tilewidth="8" tileheight="8" tilecount="60" columns="10">
+ <image source="Aesprite Files/AI Images/abandoned_house.png" trans="65ff00" width="80" height="48"/>
+</tileset>
