f1d54315e4ffdeae8eccc005e99c13f1320f4eaf
Bao Pham <47954774+SenseiBao@users.noreply.github.com>
2025-04-28 22:42:45 -0400

    Added vampire hut

--- TSX files/DungeonTileset.tsx
+++ TSX files/DungeonTileset.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.11.2" name="DungeonTileset" tilewidth="8" tileheight="8" tilecount="182" columns="14">
- <image source="../DungeonTileset.png" trans="65ff00" width="112" height="104"/>
+ <image source="../assets/tilesets/DungeonTileset.png" trans="65ff00" width="112" height="104"/>
 </tileset>
